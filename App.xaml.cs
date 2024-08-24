0d2cc722b464a45d2d0f1b87ff17fc1882ec3214
Hardy <mikewegner@gmx.net>
2024-08-24 12:17:20 +0200

    Projektdateien hinzufügen.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace brutto_netto_rechner_ui
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
