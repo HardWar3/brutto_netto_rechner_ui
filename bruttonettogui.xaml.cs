805ac5000bd8b1e337726b1fa3542ec93bae05bb
HardWar3 <mikewegner@gmx.net>
2024-09-02 21:23:29 +0200

    add window changes and panels on the gui

--- bruttonettogui.xaml.cs
+++ bruttonettogui.xaml.cs
@@ -12,13 +12,47 @@ using System.Windows.Shapes;
 namespace brutto_netto_rechner_ui
 {
     /// <summary>
-    /// Interaction logic for bruttonettogui.xaml
+    /// Interaction logic for Bruttonettogui.xaml
     /// </summary>
-    public partial class bruttonettogui : Window
+    public partial class Bruttonettogui : Window
     {
-        public bruttonettogui()
+        public Bruttonettogui()
         {
             InitializeComponent();
+        }
+
+        private void krankenversicherung_combobox_onChanged(object sender,SelectionChangedEventArgs e)
+        {
+            if (privatversicherung_comboboxItem == null)
+            {
+                return;
+            }
+
+            if (privatversicherung_comboboxItem.IsSelected)
+            {
+                privatversicherung_Panel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                privatversicherung_Panel.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void kinder_ja_radioButton_onChecked(object sender, RoutedEventArgs e)
+        {
+            if (kinder_ja_radioButton == null)
+            {
+                return;
+            }
+
+            if (kinder_ja_radioButton.IsChecked.GetValueOrDefault())
+            {
+                kinderfreibetrag_Panel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                kinderfreibetrag_Panel.Visibility = Visibility.Collapsed;
+            }
         }
     }
 }
